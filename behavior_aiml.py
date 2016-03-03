ecea6ea25838d60d5f7fc94480ecab50b38e7e10
Sebastien Dessureault <sdessureault@videotron.ca>
2016-03-03 04:45:28 +0000

    Commit avant d aller chercher les nouveau fichier .aiml anglais

--- behavior_aiml.py
+++ behavior_aiml.py
@@ -9,31 +9,36 @@ from sound_play.libsoundplay import SoundClient
 rospy.init_node('node_aiml', anonymous=True)
 rospy.loginfo("node_play_aiml")
 
-rospy.loginfo("Init son")
+rospy.loginfo("Behavior_aiml")
 soundhandle = SoundClient()
 rospy.sleep(1)
 soundhandle.stopAll()
 
-rospy.loginfo("Dire hello world")
-soundhandle.say('Hello world!')
+soundhandle.say('Testing the new A P I.')
 rospy.sleep(3)
 
+voix = soundhandle.voiceSound("Testing the new A P I.")
+voix.repeat()
+rospy.sleep(3)
+voix.stop()
+
+langue = 1
 
 # L'objet Kernel est l'interface public pour l'interpreteur AIML. 
-#k = aiml.Kernel()
+k = aiml.Kernel()
 
 # Chargement de d'un fichier de contenu
 # startup.xml charge tous les fichiers de contenu .aiml
 
-# Version anglaise
-#k.learn("/home/ubuntu/catkin_ws/src/spike/src/spike/aiml/en/startup.xml")
+if langue = 1:
+	# Version anglaise
+	k.learn("/home/ubuntu/catkin_ws/src/spike/src/spike/aiml/en/startup.xml")
+	k.respond("LOAD AIML ENGLISH")
+else:
+	# Version francaise
+	k.learn("/home/ubuntu/catkin_ws/src/spike/src/spike/aiml/fr/startup.xml") 
+	k.respond("LOAD AIML FRENCH")
 
-# Version francaise
-#k.learn("/home/ubuntu/catkin_ws/src/spike/src/spike/aiml/fr/startup.xml")
-
-
-#k.respond("load aiml b")
-
-#while True: print k.respond(raw_input("> "))
+while True: print k.respond(raw_input("Spike chat BOT> "))
 
 
--- behavior_play_sound.py
+++ behavior_play_sound.py
@@ -2,17 +2,27 @@
 import roslib
 import rospy, os, sys
 import time
+import aiml
+from sound_play.msg import SoundRequest
+from sound_play.libsoundplay import SoundClient
 
 rospy.init_node('node_play_sound', anonymous=True)
 rospy.loginfo("node_play_sound")
 
-soundAssets = '/home/pi/catkin_ws/src/spike/src/sons/'
+rospy.loginfo("Init son")
+soundhandle = SoundClient()
+rospy.sleep(1)
+soundhandle.stopAll()
+
+soundAssets = '/home/ubuntu/catkin_ws/src/spike/src/spike/sons/'
 nomWav = 'robot.wav'
 
-attente = 1 # seconds
 
+attente = 5 # seconds
 while (True):
-		os.system("mplayer " + soundAssets + nomWav)
+		#os.system("mplayer " + soundAssets + nomWav)
+		soundhandle.playWave(soundAssets + nomWav)
 		time.sleep(attente)
+
 
 
