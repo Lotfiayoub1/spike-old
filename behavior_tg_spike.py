c479eb163e6e060b14c7369fd1763eee6dfaa83f
Jean-Sebastien Dessureault <sdessureault@videotron.ca>
2017-04-26 17:16:13 +0000

    JSD Ajout Spike 2

---
+++ behavior_tg_spike.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+import rospy
+import time
+from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Joy
+
+rospy.init_node('behavior_tg_spike', anonymous=True)
+pub = rospy.Publisher('/Rosaria/cmd_vel', Twist, queue_size = 10)
+#rate = rospy.Rate(5) # hz
+global msg
+msg = Twist()
+
+
+def callback(data):
+	rospy.loginfo(rospy.get_caller_id() + " recu!")
+	# Saisie joystick
+	msg.linear.x = 0.0
+	msg.linear.y = 0.0
+	msg.linear.z = 0.0
+	msg.angular.x = 0.0
+	msg.angular.y = 0.0
+	msg.angular.z = 0.0
+
+        multiplicateur = 1.5
+
+        msg.linear.x = data.axes[4] * multiplicateur
+        msg.angular.z = data.axes[3] * multiplicateur
+
+        #vitesse = 0.1
+        #if data.buttons[3] != 0:    # Y: Avance
+        #	rospy.loginfo("Avance")
+        #        msg.linear.x = vitesse
+        #if data.buttons[0] != 0:    # A: Recule
+        #        rospy.loginfo("Recule")
+        #        msg.linear.x = -vitesse
+        #if data.buttons[2] != 0:    # X: Gauche
+        #        rospy.loginfo("Gauche")
+        #        msg.angular.x = vitesse
+        #if data.buttons[1] != 0:    # B: Droite
+        #        rospy.loginfo("Droite")
+        #        msg.angular.x = -vitesse
+        pub.publish(msg)
+        #time.sleep(3)
+
+rospy.Subscriber("/joy", Joy, callback)
+
+rospy.spin()
+
+    
